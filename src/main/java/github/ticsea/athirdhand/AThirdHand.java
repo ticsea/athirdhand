cda5f3e31da292c7a59f9836fc16a2b117d46d9f
ticsea <ticseaatlan@gmail.com>
2025-10-21 05:28:35 +0800

    Update - Simplified name of jar. - Update version to 2.0.2. - Amend profile synch...commit.

--- src/main/java/github/ticsea/athirdhand/AThirdHand.java
+++ src/main/java/github/ticsea/athirdhand/AThirdHand.java
@@ -3,14 +3,13 @@
 
 package github.ticsea.athirdhand;
 
-import static github.ticsea.athirdhand.config.ModConfig.CONFIG;
+import static github.ticsea.athirdhand.config.ModConfig.SPEC;
 
 import github.ticsea.athirdhand.client.ModKeyBinding;
 import github.ticsea.athirdhand.events.KeyPressEvent;
 import github.ticsea.athirdhand.events.ChestOpenEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -22,16 +21,15 @@ public class AThirdHand {
     public static final String MODID = "athirdhand";
 
     public AThirdHand(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
-        IEventBus forgeEvenBus = MinecraftForge.EVENT_BUS;
+        var modEventBus = context.getModEventBus();
+        var forgeEvenBus = MinecraftForge.EVENT_BUS;
 
         if (FMLEnvironment.dist==Dist.CLIENT) {
             modEventBus.addListener(ModKeyBinding::registerKeybind);
             forgeEvenBus.addListener(KeyPressEvent::onKeyPress);
-            forgeEvenBus.addListener(KeyPressEvent::renderToggleMessage);
+            forgeEvenBus.addListener(ChestOpenEvent::onChestOpen);
         }
-
-        context.registerConfig(ModConfig.Type.CLIENT, CONFIG);
-        forgeEvenBus.addListener(ChestOpenEvent::onPlayerOpenContainer);
+        context.registerConfig(ModConfig.Type.CLIENT, SPEC);
+        github.ticsea.athirdhand.config.ModConfig.loadConfig();
     }
 }
