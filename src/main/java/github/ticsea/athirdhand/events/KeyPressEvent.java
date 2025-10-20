952eeeef62bf0c454edcb1fd38410e6d6d40e8c8
ticsea <ticseaatlan@gmail.com>
2025-10-21 05:17:07 +0800

    fix: fix the issue with incorrect text prompt positioning

--- src/main/java/github/ticsea/athirdhand/events/KeyPressEvent.java
+++ src/main/java/github/ticsea/athirdhand/events/KeyPressEvent.java
@@ -1,42 +1,23 @@
 package github.ticsea.athirdhand.events;
 
-import com.mojang.blaze3d.platform.Window;
-import com.mojang.logging.LogUtils;
 import github.ticsea.athirdhand.client.ModKeyBinding;
 import github.ticsea.athirdhand.config.ModConfig;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent.Post;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.event.TickEvent.ClientTickEvent;
 import net.minecraftforge.event.TickEvent.Phase;
-import org.slf4j.Logger;
 
 public class KeyPressEvent {
-    private static final Logger LOGGER = LogUtils.getLogger();
+//    private static final Logger LOGGER = LogUtils.getLogger();
 
-    //    private static final Progress MSG_PROGRESS = new Progress(60000);
     private static final KeyMapping KEY = ModKeyBinding.getKey();
     private static final BooleanValue SWITCH = ModConfig.getSwitch();
+    private static final Minecraft mc = Minecraft.getInstance();
 
     private static final Component MSG_ENABLED = Component.translatable("athirdhand.state.on");
     private static final Component MSG_DISABLED = Component.translatable("athirdhand.state.off");
-    private static final int MSG_COLOR = 0xFFFFFF;
-    private static final int MSG_SHADOW = 1;
-    private static final int MSG_DURATION = 60;
-
-    static final Minecraft mc = Minecraft.getInstance();
-    static Font font = mc.font;
-
-    //    TODO 位置有问题
-    static Window window = mc.getWindow();
-    static int messageX = window.getGuiScaledWidth() / 2 - 22;
-    static int messageY = window.getGuiScaledHeight() - 56;
-
-    static long time;
-    static long showMessageUntilTick = 0;
 
     public static void onKeyPress(ClientTickEvent event) {
         if (event.phase!=Phase.END) return;
@@ -44,17 +25,8 @@ public class KeyPressEvent {
         while (KEY.consumeClick()) {
             SWITCH.set(!SWITCH.get());
 
-            if (mc.level!=null) {
-                showMessageUntilTick = mc.level.getGameTime() + MSG_DURATION;
-                LOGGER.debug("TELL ME THE TIEM: {}", (mc.level.getGameTime() * 50L + mc.getDeltaFrameTime() * 50L));
-            }
+            var temp = SWITCH.get() ? MSG_ENABLED : MSG_DISABLED;
+            mc.player.displayClientMessage(temp, true);
         }
-    }
-
-    public static void renderToggleMessage(Post event) {
-        if (mc.level==null || mc.level.getGameTime() >= showMessageUntilTick) return;
-
-        Component message = SWITCH.get() ? MSG_ENABLED:MSG_DISABLED;
-        event.getGuiGraphics().drawString(font, message, messageX, messageY, MSG_COLOR, MSG_SHADOW!=0);
     }
 }
